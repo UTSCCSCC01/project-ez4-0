76db1966b54abab453f1c5001e393f23387fb88a
Daniel Xu <niellxu@yahoo.ca>
2021-06-04 13:44:48 -0400

    Initialize user controller

---
+++ backend/controllers/v1/__init__.py
@@ -0,0 +1,6 @@
+"""
+This are the controllers of the application, corresponding
+to the 'C' in the 'MVC' design architecture. This is the
+first version therefore under the folder v1. In the future
+when we publish new APIs we will use v2, v3, ...
+"""
---
+++ backend/controllers/v1/user_controller.py
@@ -0,0 +1,23 @@
+"""
+This controller handles the following APIs:
+GET /api/v1/users/:id
+POST /api/v1/users
+PATCH /api/v1/users/:id
+DELETE /api/v1/users/:id
+"""
+
+
+class UserController():
+    def get():
+        pass
+    
+    def delete():
+        pass
+
+    def patch():
+        pass
+
+
+class UsersController():
+    def post():
+        pass
