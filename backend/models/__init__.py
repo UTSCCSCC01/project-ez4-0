c150e2a25da5d6f1e840e4420ba24fb9a19df607
Daniel Xu <niellxu@yahoo.ca>
2021-06-05 11:06:19 -0400

    Add document for models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,7 @@
+"""
+This is the view layer `V` in the MVC design. Access to
+database and all of the operations should be done through
+using models. Note that in flask we use models instead of
+DAO.
+"""
 from .user import User
