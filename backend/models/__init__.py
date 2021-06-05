a0346b2a0234980a3c829483f8b54255c982bc9f
Daniel Xu <niellxu@yahoo.ca>
2021-06-05 11:06:57 -0400

    Update models document

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,5 @@
 """
-This is the view layer `V` in the MVC design. Access to
+This is the view layer `M` in the MVC design. Access to
 database and all of the operations should be done through
 using models. Note that in flask we use models instead of
 DAO.
