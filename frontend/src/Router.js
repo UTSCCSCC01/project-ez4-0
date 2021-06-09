d0ea4ac20e820219369b4e983183f33cc4cdafa7
Daniel Xu <niellxu@yahoo.ca>
2021-06-09 01:46:46 -0400

    [EZ40-28] Integrate with router

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SignupPage from './pages/SignupPage/SignupPage';
+import ResetPasswordPage from './pages/ResetPasswordPage/ResetPasswordPage';
+import ResetPasswordConfirmPage from './pages/ResetPasswordConfirmPage/ResetPasswordConfirmPage';
 
 export default function AppRouter() {
   return (
@@ -7,6 +10,12 @@ export default function AppRouter() {
       <Switch>
         <Route path="/signup">
           <SignupPage/>
+        </Route>
+        <Route path="/reset">
+          <ResetPasswordPage/>
+        </Route>
+        <Route path="/reset_confirm">
+          <ResetPasswordConfirmPage/>
         </Route>
       </Switch>
     </Router>
