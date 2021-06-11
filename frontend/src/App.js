96975eb6f78f31b441bb75ebacc658ec09910078
Daniel Xu <niellxu@yahoo.ca>
2021-06-11 11:40:56 -0400

    [chore] Route user profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import "./css/App.css";
 import Router from "./Router";
-import Profile from "./pages/ProfilePage/ProfilePage.jsx";
 
 function App() {
   return (
     <div>
-      <Profile />
+      <Router/>
     </div>
   );
 }
--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -5,6 +5,7 @@ import ResetPasswordPage from './pages/ResetPasswordPage/ResetPasswordPage';
 import ResetPasswordConfirmPage from './pages/ResetPasswordConfirmPage/ResetPasswordConfirmPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import LandingPage from './pages/LandingPage/LandingPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 import UnAuthPageHeader from "./components/UnAuthPageHeader";
 
@@ -13,6 +14,9 @@ export default function AppRouter() {
     <Router>
       <UnAuthPageHeader/>
       <Switch>
+        <Route path="/profile">
+          <ProfilePage />
+        </Route>
         <Route path="/login">
           <LoginPage/>  
         </Route>
