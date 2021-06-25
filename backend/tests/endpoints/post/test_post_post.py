9f2608e8500ed0378f8adce7ea277f5c54be4219
Daniel Xu <niellxu@yahoo.ca>
2021-06-25 17:32:27 -0400

    Add tests for posts endpoint

---
+++ backend/tests/endpoints/post/test_post_post.py
@@ -0,0 +1,41 @@
+"""
+Test: POST /api/v1/posts
+"""
+from tests.utils import clear_db, clear_mongo_db, create_user
+
+
+POST_ENDPOINT = "/api/v1/posts"
+
+
+@clear_db
+@clear_mongo_db
+def test_create_post(app):
+    """
+    Create post successfully, 200
+    """
+    client = app.test_client()
+    user = create_user(app, "example@abc.com", "x1234")
+    response = client.post(
+        POST_ENDPOINT,
+        data={
+            "content": "Example post",
+            "user_id": user.id
+        }
+    )
+    assert response.status_code == 200
+
+
+@clear_db
+@clear_mongo_db
+def test_missing_body(app):
+    """
+    Create post with missing body, 422
+    """
+    client = app.test_client()
+    response = client.post(
+        POST_ENDPOINT,
+        data={
+            "content": "Example post"
+        }
+    )
+    assert response.status_code == 422
