37698ee26c21e0ee46acc4a085e23701d23aa8ba
Zhenye Zhu <67647342+ZhenyeZhu@users.noreply.github.com>
2021-06-29 11:58:19 -0400

    [EZ40-46] update job posting component

--- frontend/src/components/JobPosting.jsx
+++ frontend/src/components/JobPosting.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
-import Crayon from "./Crayon.jpg";
 
 class JobPosting extends Component {
   render() {
     return (
       <div>
         <div className="flex border ml-5 mr-5 p-3">
-          <img src={Crayon} width="30" height="30" />
-          <text className="text text-primary ml-3">It is a company name.</text>
+          <img src={this.props.img} width="30" height="30" />
+          <text className="text text-primary ml-2">{this.props.name}</text>
           {/* <span className = "ml-3">It is a time.</span> */}
           <div>
-            <p className="mt-2">
-              It is a paragraph, a very very very very very very very
-              looooooooooooong paragraph.
-            </p>
+            <p className="mt-2">{this.props.context1}</p>
           </div>
           <img
             style={{
@@ -22,7 +18,7 @@ class JobPosting extends Component {
               height: "100%",
               resizeMode: "contain",
             }}
-            src={Crayon}
+            src={this.props.context2}
             className=""
           />
 
