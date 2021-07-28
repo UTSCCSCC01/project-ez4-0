458227beeb76f9ff64082f7a76bcca6614ca250e
Daniel Xu <niellxu@yahoo.ca>
2021-07-28 16:45:42 -0400

    [EZ40-81] Update endpoint for course query

--- backend/controllers/v1/course_controller.py
+++ backend/controllers/v1/course_controller.py
@@ -3,11 +3,12 @@ This controller handles the following APIs:
 - GET /api/v1/courses/:id
 - GET /api/v1/courses
 """
-from flask_apispec import doc, marshal_with
+from flask_apispec import doc, marshal_with, use_kwargs
 from schemas import (
     CourseSchema,
     GetCoursesSchema
 )
+from schemas.course import CourseQuerySchema
 from .base_controller import BaseController
 from models import Course
 
@@ -26,6 +27,20 @@ class CourseController(BaseController):
 @doc(tags=["Course"])
 class CoursesController(BaseController):
     @doc(description="Get all available courses")
+    @use_kwargs(CourseQuerySchema)
     @marshal_with(GetCoursesSchema)
-    def get(self, **_):
-        return {"courses": Course.query.all()}
+    def get(self, **kwargs):
+        keyword = kwargs.get("keyword")
+        category = kwargs.get("category")
+        if keyword and category:
+            courses = Course.query.filter(
+                Course.name.like(f"%{keyword}%"),
+                Course.category == category
+            )
+        elif keyword:
+            courses = Course.query.filter(Course.name.like(f"%{keyword}%"))
+        elif category:
+            courses = Course.query.filter(Course.category == category)
+        else:
+            courses = Course.query.all()
+        return {"courses": courses}
