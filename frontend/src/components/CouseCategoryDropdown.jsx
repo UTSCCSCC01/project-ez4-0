70b8bd8221350e5428b90b0d05b99822050b09ed
Zhenye Zhu <67647342+ZhenyeZhu@users.noreply.github.com>
2021-07-29 22:08:29 -0400

    [EZ40-83] change category

--- frontend/src/components/CouseCategoryDropdown.jsx
+++ frontend/src/components/CouseCategoryDropdown.jsx
@@ -8,7 +8,7 @@ export default function Example() {
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="mt-7 inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            Options
+            Categorys
             <ChevronDownIcon
               className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
